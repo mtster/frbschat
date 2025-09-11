fde814400873e959341f4c152cfd9e2be6bc0181
mtster <mate.topchishvili@outlook.com>
2025-09-12 02:20:58 +0400

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,3 +1,9 @@
+
+// firebase-config.js
+// replace values with your Firebase project settings
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-app.js";
+import { getDatabase } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-database.js";
+
 // firebase-config.js
 const firebaseConfig = {
   apiKey: "AIzaSyA-FwUy8WLXiYtT46F0f59gr461cEI_zmo",
@@ -8,3 +14,6 @@ const firebaseConfig = {
   messagingSenderId: "969101904718",
   appId: "1:969101904718:web:8dcd0bc8690649235cec1f"
 };
+
+const app = initializeApp(firebaseConfig);
+export const db = getDatabase(app);
