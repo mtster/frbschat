6ed09afb6ce370bc66bbeeabf42a1817f36d95c5
mtster <mate.topchishvili@outlook.com>
2025-09-12 02:05:02 +0400

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,10 @@
+// firebase-config.js
+const firebaseConfig = {
+  apiKey: "AIzaSyA-FwUy8WLXiYtT46F0f59gr461cEI_zmo",
+  authDomain: "protocol-chat-b6120.firebaseapp.com",
+  databaseURL: "https://protocol-chat-b6120.firebaseio.com",
+  projectId: "protocol-chat-b6120",
+  storageBucket: "YOUR_PROJECT_ID.appspot.com",
+  messagingSenderId: "969101904718",
+  appId: "1:969101904718:web:8dcd0bc8690649235cec1f"
+};
