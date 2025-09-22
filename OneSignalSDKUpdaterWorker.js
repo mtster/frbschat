28683ea3204e0595cc3f9b051868dc364b361d2a
mtster <mate.topchishvili@outlook.com>
2025-09-22 18:22:59 +0400

    Create OneSignalSDKUpdaterWorker.js

---
+++ OneSignalSDKUpdaterWorker.js
@@ -0,0 +1,3 @@
+// OneSignalSDKUpdaterWorker.js
+// Leave this file at the top-level root of your site (https://frbschat.pages.dev/OneSignalSDKUpdaterWorker.js)
+importScripts("https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.sw.js");
